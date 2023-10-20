ce72cdbffdc0d10439093195bd06f7d90f6a7881
Rachmawati <115967419+rachmawatii16@users.noreply.github.com>
2023-10-20 18:14:59 +0700

    Add files via upload

---
+++ Latihan2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kalkulator Sederhana</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <!-- style -->
+    <style>
+        .result-box {
+            border: 5px solid #f5efdf;
+            background-color: #fff;
+            padding: 10px;
+            margin: 10px auto;
+            width: 800px;
+            text-align: left;
+            border-radius: 5px;
+        }
+        body {
+            background-color: #f7f7f7;
+        }
+        .container {
+            background-color: #f5efdf;
+            border-radius: 8px;
+            margin-top: 18px;
+            padding: 18px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .btn-primary {
+            background-color: #000000;
+            border: none;
+        }
+        .btn-primary:hover {
+            background-color: #fff;
+        }
+        .icon {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2 class="mb-3">Selamat datang di latihan 2</h2>
+        <p>Ini adalah kalkulator sederhana</p
+        <!-- input 1 -->
+        <p>Bilangan Pertama</p>
+        <form method="POST" action="">
+            <div class="form-group">
+                <input type="text" class="form-control" name="bilangan1" placeholder="Bilangan pertama">
+            </div>
+            <!-- operasi -->
+            <p>Pilih Operasi</p>
+            <div class="form-group">
+                <select class="form-control" name="operasi">
+                    <option value="tambah">+</option>
+                    <option value="kurang">-</option>
+                    <option value="kali">*</option>
+                    <option value="bagi">/</option>
+                </select>
+            </div>
+            <!-- input 2 -->
+            <p>Bilangan Kedua</p>
+            <div class="form-group">
+                <input type="text" class="form-control" name="bilangan2" placeholder="Bilangan kedua">
+            </div>
+            <button type="submit" class="btn btn-primary" name="hitung"><i class="fas fa-calculator icon"></i> Hitung</button>
+        </form>
+        <?php
+        // operasi
+        if (isset($_POST['hitung'])){
+            $bilangan1 = $_POST['bilangan1'];
+            $bilangan2 = $_POST['bilangan2'];
+            $operasi = $_POST['operasi'];
+            if ($operasi == "tambah") {
+                $hasil = $bilangan1 + $bilangan2;
+            } elseif ($operasi == "kurang") {
+                $hasil = $bilangan1 - $bilangan2;
+            } elseif ($operasi == "kali") {
+                $hasil = $bilangan1 * $bilangan2;
+            } elseif ($operasi == "bagi") {
+                if ($bilangan2 == 0) {
+                    echo "<p class='result'>Error</p>";
+                } else {
+                    $hasil = $bilangan1 / $bilangan2;
+                }
+            }
+            // hasil
+            if (isset($hasil)) {
+                echo "<div class='result-box'>";
+                echo "<p class='result'>Hasil: $hasil</p>";
+                echo "</div>";
+            }
+        }
+        ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latihan 1 PHP</title>
+</head>
+<body>
+	<h1>Selamat datang</h1>
+	<?php
+	echo 'selamat datang';
+	$input_angka=4;
+	$input_string='string';
+	echo 'string';
+	?>
+</body>
+</html>
